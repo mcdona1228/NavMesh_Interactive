2024af056cab006c417e438765485fd2c0fc9dc4
mcdona1228 <61477480+mcdona1228@users.noreply.github.com>
2022-03-07 08:58:21 -0700

    Scripts

---
+++ Scripts/Playermovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Playermovement : MonoBehaviour
+{
+    public float forwardForce = 0f;
+    public Rigidbody rb;
+    int jumpcount = 0;
+    public int MaxJumps = 2;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Fixed update because physics
+    void Update()
+    {
+        //rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(500 * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-500 * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, 1000 * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -1000 * Time.deltaTime);
+        }
+        if (Input.GetKeyDown("e"))
+        {
+            if (jumpcount> 0)
+            {
+                rb.AddForce(0, 320 * Time.deltaTime, 0, ForceMode.VelocityChange);
+                jumpcount--;
+            }
+            
+        }
+       
+    }
+}
---
+++ Scripts/RandomPosition.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class RandomPosition : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(RePositionWithDelay());
+    }
+    
+    IEnumerator RePositionWithDelay()
+    {
+        while (true)
+        {
+            SetRandomPosition();
+            yield return new WaitForSeconds(100f);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        SetRandomPosition();
+    }
+    void SetRandomPosition()
+    {
+        float x = Random.Range(-10f, 10f);
+        float z = Random.Range(-10f, 10f);
+        Debug.Log("X,Z: " + x.ToString("F2") + ", " + z.ToString("F2"));
+        transform.position = new Vector3(x, 0.0f, z);
+    }
+}
